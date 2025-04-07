6253a2e36d169ae2eae354fc4b85288096b4bf8c
Dzengiz Prentic <Dzengiz.prentic@hotmail.com>
2025-04-07 15:36:05 +0200

    Remove commented-out User class properties for cleaner code

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,3 @@
-// public class User
-// {
-//     public Guid Id { get; set; }
-//     public string Username { get; set; } = string.Empty;
-//     public string PasswordHash { get; set; } = string.Empty;
-//     public List<Folder> Folders { get; set; } = [];
-//     public List<File> Files { get; set; } = [];
-// }
-
 using Microsoft.AspNetCore.Identity;
 
 public class User : IdentityUser
